3e4570ba32b41db70fff9d5c176de2ece643ba03
Erfan <erfankobayashi@gmail.com>
2025-09-05 19:20:02 +0330

    fix: correct sidebar/nav layout and scrolling behavior on dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import SideNav from "@/app/ui/dashboard/sidenav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-dvh flex flex-col bg-background lg:flex-row lg:overflow-hidden">
-      <div className="w-full flex-none fixed bottom-0 lg:relative lg:w-64">
+    <div className="min-h-dvh flex flex-col bg-background lg:flex-row">
+      <div className="w-full fixed bottom-0 bg-background border-t-1 border-gray-200 dark:border-neutral-800 lg:h-dvh lg:w-64 lg:overflow-y-auto">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 lg:overflow-y-auto lg:p-12">{children}</div>
+      <div className="flex-grow p-6 pb-26 lg:overflow-y-auto lg:p-12 lg:ml-64">{children}</div>
     </div>
   );
 }
