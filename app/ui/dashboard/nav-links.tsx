d8333f94d4561679c859e8c4b94712e4358f4d3d
Erfan Azarmi <erfanazarmi1@gmail.com>
2025-09-07 13:16:50 +0330

    feat: implement add transaction form page UI

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { HomeIcon } from "@heroicons/react/24/outline";
+import { HomeIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import clsx from "clsx";
 
 const links = [
   {name: "Home", href: "/dashboard", icon: HomeIcon },
+  {name: "Add", href: "/dashboard/add", icon: PlusIcon },
 ];
 
 export default function NavLinks() {
