27b5ad503400859adef20d65bb880422009f2272
Erfan <erfankobayashi@gmail.com>
2025-12-23 21:20:46 +0330

    style: refine stats cards layout and responsiveness

--- app/ui/dashboard/total-stats-cards.tsx
+++ app/ui/dashboard/total-stats-cards.tsx
@@ -16,41 +16,46 @@ export default function TotalStatsCards({ from, to }: { from: string; to: string
   if (!stats) return <></>;
 
   return (
-    <div className="flex flex-col gap-3 md:flex-row lg:gap-5">
-      <div className="flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
-        <div className="flex gap-2 font-medium text-sm p-1 pb-3 lg:p-2 lg:pb-3">
-          <PlusCircleIcon className="w-5 text-green-500" />
-          Income
+    <div className="flex flex-col gap-3 md:flex-row">
+      <div className="flex flex-1 flex-col md:flex xl:flex-row gap-3">
+        <div className="flex lg:flex-col justify-between flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
+          <div className="flex items-center gap-2 font-medium text-sm p-1 lg:p-2 lg:pb-3">
+            <PlusCircleIcon className="w-5 text-green-500" />
+            Income
+          </div>
+          <div className="bg-background px-3 py-1 flex justify-center items-center text-lg lg:text-2xl rounded-md lg:p-6">
+            {formatNumberWithSpaces(stats.total_income)}
+          </div>
         </div>
-        <div className="bg-background p-5 flex justify-center items-center text-2xl rounded-md lg:p-6">
-          {formatNumberWithSpaces(stats.total_income)}
-        </div>
-      </div>
-      <div className="flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
-        <div className="flex gap-2 font-medium text-sm p-1 pb-3 lg:p-2 lg:pb-3">
-          <MinusCircleIcon className="w-5 text-red" />
-          Expense
-        </div>
-        <div className="bg-background p-5 flex justify-center items-center text-2xl rounded-md lg:p-6">
-          {formatNumberWithSpaces(stats.total_expense)}
-        </div>
-      </div>
-      <div className="flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
-        <div className="flex gap-2 font-medium text-sm p-1 pb-3 lg:p-2 lg:pb-3">
-          <EllipsisHorizontalCircleIcon className="w-5 text-neutral-500" />
-          Total
-        </div>
-        <div className="bg-background p-5 flex justify-center items-center text-2xl rounded-md lg:p-6">
-          {formatNumberWithSpaces(stats.total_amount)}
+        <div className="flex lg:flex-col justify-between flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
+          <div className="flex items-center gap-2 font-medium text-sm p-1 lg:p-2 lg:pb-3">
+            <MinusCircleIcon className="w-5 text-red" />
+            Expense
+          </div>
+          <div className="bg-background px-3 py-1 flex justify-center items-center text-lg lg:text-2xl rounded-md lg:p-6">
+            {formatNumberWithSpaces(stats.total_expense)}
+          </div>
         </div>
       </div>
-      <div className="flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
-        <div className="flex gap-2 font-medium text-sm p-1 pb-3 lg:p-2 lg:pb-3">
-          <CreditCardIcon className="w-5 text-neutral-500" />
-          All-Time Total
+
+      <div className="flex flex-1 flex-col md:flex xl:flex-row gap-3">
+        <div className="flex lg:flex-col justify-between flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
+          <div className="flex items-center gap-2 font-medium text-sm p-1 lg:p-2 lg:pb-3">
+            <EllipsisHorizontalCircleIcon className="w-5 text-neutral-500" />
+            Total
+          </div>
+          <div className="bg-background px-3 py-1 flex justify-center items-center text-lg lg:text-2xl rounded-md lg:p-6">
+            {formatNumberWithSpaces(stats.total_amount)}
+          </div>
         </div>
-        <div className="bg-background p-5 flex justify-center items-center text-2xl rounded-md lg:p-6">
-          {formatNumberWithSpaces(stats.all_time_total)}
+        <div className="flex lg:flex-col justify-between flex-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900 lg:p-3">
+          <div className="flex items-center gap-2 font-medium text-sm p-1 lg:p-2 lg:pb-3">
+            <CreditCardIcon className="w-5 text-neutral-500" />
+            All-Time Total
+          </div>
+          <div className="bg-background px-3 py-1 flex justify-center items-center text-lg lg:text-2xl rounded-md lg:p-6">
+            {formatNumberWithSpaces(stats.all_time_total)}
+          </div>
         </div>
       </div>
     </div>
